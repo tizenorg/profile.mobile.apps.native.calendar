813f6cd0bf754af8e06ebac90b2b280dd4f3750a
Yevgeniy Kolomiets <y.kolomiets@samsung.com>
2016-02-23 17:29:30 +0200

    TizenRefApp-5713 Prepare GBS & SDK project for new Calendar
    Change-Id: Ie48cdfb9c1fd00311a083c848caa51858a0e901c
    Signed-off-by: Yevgeniy Kolomiets <y.kolomiets@samsung.com>

---
+++ apps-common/inc/Ui/Window.h
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#ifndef UI_WINDOW_H
+#define UI_WINDOW_H
+
+#include "Ui/Control.h"
+#include <functional>
+
+namespace Ui
+{
+	class View;
+
+	/**
+	 * @brief Application main window
+	 */
+	class EXPORT_API Window : public Control
+	{
+	public:
+		/**
+		 * @brief Called when "back" button was pressed and wasn't handled by any view.
+		 * @return true to lower the window, otherwise false.
+		 */
+		typedef std::function<bool()> BackCallback;
+
+		Window();
+
+		/**
+		 * @return Conformant Evas_Object.
+		 */
+		Evas_Object *getConformant() const;
+
+		/**
+		 * @return Basic Window layout Evas_Object.
+		 */
+		Evas_Object *getBaseLayout() const;
+
+		/**
+		 * @brief Set "back" button callback.
+		 * @param[in]   callback    "back" button callback
+		 */
+		void setBackCallback(BackCallback callback);
+
+		/**
+		 * @brief Unset "back" button callback.
+		 */
+		void unsetBackCallback();
+
+		/**
+		 * @brief Attach main View to be displayed in the Window.
+		 * @param[in]   view    Window main View
+		 */
+		void attachView(View *view);
+
+	private:
+		virtual Evas_Object *onCreate(Evas_Object *parent) override;
+		void onBackPressed(Evas_Object *obj, void *eventInfo);
+		void onMenuPressed(Evas_Object *obj, void *eventInfo);
+
+		Evas_Object *m_Conform;
+		Evas_Object *m_Layout;
+		View *m_MainView;
+		BackCallback m_OnBackPressed;
+	};
+}
+
+#endif /* UI_WINDOW_H */
---
+++ apps-common/res/apps-common/edje/AppsCommonThumbnail.h
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#ifndef APPS_COMMON_THUMBNAIL_H
+#define APPS_COMMON_THUMBNAIL_H
+
+#include "AppsCommonPath.h"
+
+#define APPS_COMMON_THUMBNAIL_EDJ       APPS_COMMON_EDJ_DIR"apps-common-thumbnail.edj"
+
+#define THUMBNAIL_DEFAULT_MEDIUM        APPS_COMMON_IMG_DIR"contacts_caller_id_default.png"
+#define THUMBNAIL_DEFAULT_SMALL         APPS_COMMON_IMG_DIR"contacts_caller_id_default_02.png"
+#define THUMBNAIL_DEFAULT_LARGE         APPS_COMMON_IMG_DIR"contacts_caller_id_default_03.png"
+
+#define THUMBNAIL_BG_MEDIUM             APPS_COMMON_IMG_DIR"contacts_caller_id_default_bg.png"
+#define THUMBNAIL_BG_SMALL              APPS_COMMON_IMG_DIR"contacts_caller_id_default_bg_02.png"
+#define THUMBNAIL_BG_LARGE              APPS_COMMON_IMG_DIR"contacts_caller_id_default_bg_03.png"
+
+#define GROUP_THUMBNAIL_SMALL           "thumbnail_small"
+#define GROUP_THUMBNAIL_MEDIUM          "thumbnail_medium"
+#define GROUP_THUMBNAIL_LARGE           "thumbnail_large"
+
+#define GROUP_THUMBNAIL_DEFAULT_SMALL   "default_thumbnail_small"
+#define GROUP_THUMBNAIL_DEFAULT_MEDIUM  "default_thumbnail_medium"
+#define GROUP_THUMBNAIL_DEFAULT_LARGE   "default_thumbnail_large"
+
+#define THUMBNAIL_SMALL_SIZE 98
+#define THUMBNAIL_MEDIUM_SIZE 166
+#define THUMBNAIL_LARGE_SIZE 240
+
+#endif /* APPS_COMMON_THUMBNAIL_H */
