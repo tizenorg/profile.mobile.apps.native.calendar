b68e4f45af0beda0d4fa5ceab15bda324f0f4c6c
Yevgeniy Kolomiets <y.kolomiets@samsung.com>
2016-04-07 15:54:58 +0300

    Add Agenda layout; Add EventList
    Change-Id: I76c1070dab21fcdac1243d3b54b03da19a9ea03b
    Signed-off-by: Yevgeniy Kolomiets <y.kolomiets@samsung.com>

---
+++ calendar-app/res/agenda/edje/inc/agenda_view.h
@@ -0,0 +1,6 @@
+#define AGENDA_VIEW		"agenda_view"
+
+#define MONTH_CONTROL_H 694
+
+#define PART_MONTH_CONTROL	"swallow.month"
+#define PART_LIST_CONTROL	"swallow.list"
---
+++ calendar-app/src/View/Common/EventListItem.cpp
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include "View/Common/EventListItem.h"
+#include "common/edje/inc/UiCommon.h"
+#include "Common.h"
+
+#include "Utils/Logger.h"
+
+#include <string.h>
+#include <app_i18n.h>
+
+using namespace View::Common;
+using namespace Ui;
+
+EventListItem::EventListItem()
+{
+}
+
+Elm_Genlist_Item_Class *EventListItem::getItemClass() const
+{
+	TRACE;
+	static Elm_Genlist_Item_Class itc = createItemClass(EVENT_LIST_ITEM);
+	return &itc;
+}
+
+char *EventListItem::getText(Evas_Object *parent, const char *part)
+{
+	TRACE;
+	DBG("Part: %s", part);
+	//TODO: Return text from EventInstancePart
+
+	if (PART_IS(PART_TITLE)){
+		return strdup(_("IDS_CLD_MBODY_MY_EVENT"));
+	} else if (PART_IS(PART_DAY)) {
+		return strdup("17");
+	} else if (PART_IS(PART_WEEKDAY)) {
+		return strdup("Mon");
+	} else if (PART_IS(PART_TIME)) {
+		return strdup("10:00 - 11:00");
+	} else if (PART_IS(PART_LOCATION)) {
+		return strdup("Kyiv city");
+	} else {
+		return NULL;
+	}
+}
+
+void EventListItem::onRealized()
+{
+	//TODO: If event does not have location, emit signal to collapse location textblock
+}
+
+void EventListItem::onUnrealized()
+{
+	//TODO: Unsubscribe from event data provider update/remove callbacks
+}
