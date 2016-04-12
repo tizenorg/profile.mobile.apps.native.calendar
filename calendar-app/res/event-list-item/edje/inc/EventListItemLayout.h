6de6bd080c8e0125c674f0f5d4772c1f2c5a9927
Yevgeniy Kolomiets <y.kolomiets@samsung.com>
2016-04-12 18:09:44 +0300

    Add MonthControl class, rename resources, change namespaces
    Change-Id: I9dd75d9a60926e0c4be89b8cb980f52cfa936022
    Signed-off-by: Yevgeniy Kolomiets <y.kolomiets@samsung.com>

---
+++ calendar-app/res/event-list-item/edje/inc/EventListItemLayout.h
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2016 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#define PADDING_VIEW_DEFAULT_SIZE 0
+#define PADDING_EVENT_ITEM_DEFAULT_SIZE 32
+
+#define PART_DAY		"text.day"
+#define PART_WEEKDAY	"text.weekday"
+#define PART_TITLE		"text.title"
+#define PART_TIME		"text.time"
+#define PART_LOCATION	"text.location"
+
+#define PART_COLOR_BAR	"swallow.color.bar"
+#define PART_CHECKBOX	"swallow.checkbox"
+
+#define PART_PADDING_TEXT_LEFT "padding.text.left"
+
+#define EVENT_LIST_ITEM_LAYOUT	"event_list_item_layout"
+#define EVENT_LIST_ITEM_LAYOUT_PATH "event-list-item/edje/event-list-item-layout.edj"
+#define COLOR_BAR_PATH "event-list-item/images/bg_colorbar.#.png"
+
+#define ORANGE_BAR 224 167 79 255
+#define PURPLE_BAR 141 17 27 255
