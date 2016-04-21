50c2d7f5f40122d6cd866704e462afbe0c081250
Yevgeniy Kolomiets <y.kolomiets@samsung.com>
2016-04-21 17:35:10 +0300

    Add event model and datetime classes
    Change-Id: I416e3ebcfd8f5097d12a4822907cbd2bf8829eb7
    Signed-off-by: Yevgeniy Kolomiets <y.kolomiets@samsung.com>

---
+++ lib-common/inc/Common/Model/DateTime.h
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#ifndef COMMON_MODEL_DATE_TIME_H
+#define COMMON_MODEL_DATE_TIME_H
+
+#include <string>
+#include <calendar.h>
+
+namespace Common
+{
+	namespace Model
+	{
+		/**
+		 * @brief DateTime model
+		 */
+		class DateTime
+		{
+		public:
+			DateTime();
+
+		private:
+			calendar_time_s m_Time;
+
+		};
+	}
+}
+
+#endif /* COMMON_MODEL_DATE_TIME_H */
