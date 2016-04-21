fac7cf77c6773856424369d98c768b848fed6a07
Yevgeniy Kolomiets <y.kolomiets@samsung.com>
2016-04-21 17:35:10 +0300

    Add event model and datetime classes
    Change-Id: I416e3ebcfd8f5097d12a4822907cbd2bf8829eb7
    Signed-off-by: Yevgeniy Kolomiets <y.kolomiets@samsung.com>

---
+++ lib-common/src/Common/Model/Event.cpp
@@ -0,0 +1,92 @@
+/*
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include "Common/Model/DateTime.h"
+#include "Common/Model/Event.h"
+
+using namespace Common::Model;
+
+Event::Event(calendar_record_h record)
+		:m_Record(record),
+		 m_Summary(nullptr),
+		 m_Description(nullptr),
+		 m_Location(nullptr)
+{
+	calendar_record_get_int(m_Record, _calendar_event.id, &m_Id);
+	calendar_record_get_str(m_Record, _calendar_event.summary, &m_Summary);
+	calendar_record_get_str(m_Record, _calendar_event.description, &m_Description);
+	calendar_record_get_str(m_Record, _calendar_event.location, &m_Location);
+}
+
+Event::Event()
+{
+	calendar_record_create(_calendar_event._uri, &m_Record);
+}
+
+Event::~Event()
+{
+	calendar_record_destroy(m_Record, true);
+	if(m_Summary)
+	{
+		free(m_Summary);
+	}
+
+	if(m_Description)
+	{
+		free(m_Description);
+	}
+
+	if(m_Location)
+	{
+		free(m_Location);
+	}
+}
+
+int Event::getId()
+{
+	return m_Id;
+}
+
+char *Event::getSummary()
+{
+	return m_Summary;
+}
+
+char *Event::getDescription()
+{
+	return m_Description;
+}
+
+char *Event::getLocation()
+{
+	return m_Location;
+}
+
+const DateTime &Event::getTimeStart()
+{
+	return m_TimeStart;
+}
+
+const DateTime &Event::getTimeEnd()
+{
+	return m_TimeEnd;
+}
+
+void Event::onChanged()
+{
+	//TODO: Record changed
+}
