77f65a7d4f40c5707fcff2fa1b77ce21c5eb921a
Yevgeniy Kolomiets <y.kolomiets@samsung.com>
2016-04-22 14:42:26 +0300

    Update apps-common, fix log tag
    Change-Id: Id7d3cd276b158ae1fe402c8c91332ed20aa39862
    Signed-off-by: Yevgeniy Kolomiets <y.kolomiets@samsung.com>

---
+++ lib-apps-common/inc/Ux/SelectItem.h
@@ -0,0 +1,90 @@
+/*
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#ifndef UX_SELECT_ITEM_H
+#define UX_SELECT_ITEM_H
+
+#include "Ui/GenlistCheckItem.h"
+#include "Ux/SelectTypes.h"
+
+namespace Ux
+{
+	/**
+	 * @brief Genlist item for SelectView that supports selection mode switching.
+	 */
+	class EXPORT_API SelectItem : public Ui::GenlistCheckItem
+	{
+	public:
+		SelectItem();
+
+		/**
+		 * @return Item selection mode.
+		 */
+		SelectMode getSelectMode() const;
+
+		/**
+		 * @brief Set item selection mode.
+		 */
+		void setSelectMode(SelectMode selectMode);
+
+		/**
+		 * @return Selection result associated with the item.
+		 */
+		SelectResult getSelectResult() const;
+
+		/**
+		 * @brief Set custom selection result to override the default result.
+		 */
+		void setCustomResult(SelectResult result);
+
+		/**
+		 * @brief Unset custom selection result to use default result.
+		 */
+		void unsetCustomResult();
+
+	protected:
+		/**
+		 * @return Default selection result associated with the item.
+		 */
+		virtual SelectResult getDefaultResult() const = 0;
+
+		/**
+		 * @see GenlistItem::getContent()
+		 */
+		virtual Evas_Object *getContent(Evas_Object *parent, const char *part) override;
+
+		/**
+		 * @see GenlistItem::onSelected()
+		 */
+		virtual void onSelected() override;
+
+		/**
+		 * @brief Called when selection mode was changed.
+		 * @param[in]   selectMode  New selection mode
+		 */
+		virtual void onSelectModeChanged(SelectMode selectMode) { }
+
+	private:
+		friend class SelectView;
+
+		SelectMode m_SelectMode;
+		SelectResult m_CustomResult;
+		bool m_HasCustomResult;
+	};
+}
+
+#endif /* UX_SELECT_ITEM_H */
---
+++ lib-apps-common/inc/Ux/SelectTypes.h
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#ifndef UX_SELECT_TYPES_H
+#define UX_SELECT_TYPES_H
+
+#include "Utils/Range.h"
+#include <functional>
+
+namespace Ux
+{
+	/**
+	 * @brief Determines how items can be selected.
+	 */
+	enum SelectMode
+	{
+		SelectNone,     /**< Selection is disabled */
+		SelectSingle,   /**< Only one item can be selected */
+		SelectMulti     /**< Multiple items can be selected */
+	};
+
+	/**
+	 * @brief Selection result.
+	 */
+	struct SelectResult
+	{
+		unsigned type;  /**< Result type (depends on result source) */
+		union Value
+		{
+			Value(void *data) : data(data) { }
+			Value(int id) : id(id) { }
+			void *data; /**< Result data */
+			int id;     /**< Result data ID */
+		} value;        /**< Result value (depends on type) */
+	};
+
+	/**
+	 * @brief Range of consecutive selection results.
+	 */
+	typedef Utils::Range<SelectResult *> SelectResults;
+
+	/**
+	 * @brief Callback to be called when selection is done.
+	 * @param[in]   Selection results
+	 * @return Whether the component that provided the selection should be destroyed.
+	 */
+	typedef std::function<bool(SelectResults)> SelectCallback;
+
+	/**
+	 * @brief Callback to be called when cancel is pressed.
+	 * @return Whether the component that provided the selection should be destroyed.
+	 */
+	typedef std::function<bool()> CancelCallback;
+}
+
+#endif /* UX_SELECT_TYPES_H */
