f585006af261117a2506d5ba70bb53b2886b4a01
Aleksandr Sapozhnik <a.sapozhnik@samsung.com>
2016-05-20 14:48:35 +0300

    Updated cli build files
    Change-Id: I0e61a97dd297cbcffa16b5fd88252cde404243ce
    Signed-off-by: Aleksandr Sapozhnik <a.sapozhnik@samsung.com>

--- SDK-build/calendar/Build/flags.mk
+++ SDK-build/calendar/Build/flags.mk
@@ -9,7 +9,7 @@ COMPILE_FLAGS = $(DEBUG_OP) $(OPTIMIZATION_OP)
 
 CPP_COMPILE_FLAGS = $(CPP_DEBUG_OP) $(CPP_OPTIMIZATION_OP) -Wall -c -fmessage-length=0 -std=c++0x -fPIC 
 
-LINK_FLAGS = -Xlinker -rpath=$$"ORIGIN"/../lib -shared -Wl,--no-undefined 
+LINK_FLAGS = -shared -Wl,--no-undefined 
 
 AR_FLAGS = 
 
