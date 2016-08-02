5f33f96fb9196430c8a362a55a5d76fd95c52722
Yevgeniy Kolomiets <y.kolomiets@samsung.com>
2016-08-02 12:01:20 +0300

    TizenRefApp-6787 Prepare documentation for calendar-common/main/model #5 (Settigns/Locale managers)
    Change-Id: I4b8ecc5b193502ed12606f10665b758d5779a2e2
    Signed-off-by: Yevgeniy Kolomiets <y.kolomiets@samsung.com>

--- common/model/CalLocaleManager.h
+++ common/model/CalLocaleManager.h
@@ -32,24 +32,13 @@
 class WAPP_ASSIST_EXPORT CalLocaleManager
 {
 SINGLETON_IDIOM(CalLocaleManager);
-protected:
-	CalLocaleManager();
-	virtual ~CalLocaleManager();
-private:
-	WDISABLE_COPY_AND_ASSIGN(CalLocaleManager);
 
 public:
 
-	bool isRTL();
-	void updateLocaleForEvasObj();
-	void setEvasObjForRTL(Evas_Object* obj);
-
-	void setTimeZone(const std::string& timeZone);
-
-	void getTimeZone(std::string& timeZone);
-
-	void updateRegion(void);
-
+	/**
+	 * @brief The date format enum.
+	 *
+	 */
 	enum DateFormat
 	{
 		DATEFORMAT_NONE = -1,
@@ -80,6 +69,11 @@ public:
 		DATEFORMAT_25,		//"EEEEEE" /* day of week "Tu" "Su" */
 		DATEFORMAT_END,
 	};
+
+	/**
+	 * @brief The time format enum.
+	 *
+	 */
 	enum TimeFormat
 	{
 		TIMEFORMAT_NONE = -1,
@@ -93,32 +87,248 @@ public:
 		TIMEFORMAT_8,		//"Hm" /* 7:58 AM */
 		TIMEFORMAT_END,
 	};
-	void getDateTimeText(const DateFormat dateFormat, const TimeFormat timeFormat, const CalDateTime& dateTime, std::string& text);
-	void getDateTimeText(const char* timezone, const DateFormat dateFormat, const TimeFormat timeFormat, const CalDateTime& dateTime, std::string& text);
-	void getDateText(const DateFormat dateFormat, const CalDate& date, std::string& text);
 
-	long long int getUtime(struct tm& tm);
+	/**
+	 * @brief Check whether RTL is tured ON.
+	 *
+	 * @return true is RTL enabled, otherwise false.
+	 *
+	 */
+	bool isRTL();
+
+	/**
+	 * @brief Update for EFL.
+	 *
+	 * @see elm_language_set().
+	 *
+	 */
+	void updateLocaleForEvasObj();
+
+	/**
+	 * @brief Set EvasObj for RTL.
+	 *
+	 * @param obj    EvasObj
+	 *
+	 */
+	void setEvasObjForRTL(Evas_Object* obj);
+
+	/**
+	 * @brief Set time zone.
+	 *
+	 * @param[in] timeZone     timeZone
+	 *
+	 */
+	void setTimeZone(const std::string &timeZone);
+
+	/**
+	 * @brief Get time zone.
+	 *
+	 * @param[out] timeZone      time zone.
+	 *
+	 */
+	void getTimeZone(std::string &timeZone);
+
+	/**
+	 * @brief Update region settings.
+	 *
+	 */
+	void updateRegion(void);
+
+	/**
+	 * @brief Get date and time as text.
+	 *
+	 * @param[in]  dateFormat         data format, @see DateFormat
+	 * @param[in]  timeFormat         time format, @see TimeFormat
+	 * @param[in]  dateTime           datatime
+	 * @param[out] text               text buffer
+	 *
+	 */
+	void getDateTimeText(const DateFormat dateFormat, const TimeFormat timeFormat, const CalDateTime &dateTime, std::string &text);
+
+	/**
+	 * @brief Get date and time for time zone as text.
+	 *
+	 * @param[in]  timezone         time zone
+	 * @param[in]  dateFormat       data format, @see DateFormat
+	 * @param[in]  timeFormat       time format, @see TimeFormat
+	 * @param[in]  dateTime         datatime
+	 * @param[out] text             text buffer
+	 *
+	 */
+	void getDateTimeText(const char* timezone, const DateFormat dateFormat, const TimeFormat timeFormat, const CalDateTime &dateTime, std::string &text);
+
+	/**
+	 * @brief Get date as text.
+	 *
+	 * @param[in]  dateFormat       data format, @see DateFormat
+	 * @param[in]  dateTime         datatime
+	 * @param[out] text             text buffer
+	 *
+	 */
+	void getDateText(const DateFormat dateFormat, const CalDate &date, std::string &text);
+
+	/**
+	 * @brief Get UTIME from TM structure.
+	 *
+	 * @param[in] tm       TM structure
+	 *
+	 * @return UTIME value.
+	 *
+	 */
+	long long int getUtime(struct tm &tm);
+
+	/**
+	 * @brief Get UTIME from TM structure for time zone.
+	 *
+	 * @param[in] timezone        time zone
+	 * @param[in] tm              TM structure
+	 *
+	 * @return UTIME value.
+	 *
+	 */
 	long long int getUtimeFromTm(const char *timezone, const struct tm &tm);
+
+	/**
+	 * @brief Get TM structure from UTIME.
+	 *
+	 * @param[in]  timezone      time zone
+	 * @param[in]  utime         UTIME
+	 * @param[out] tm            TM structure
+	 *
+	 */
 	void getTmFromUtime(const char *timezone, const long long int utime, struct tm &tm);
+
+	/**
+	 * @brief Get week day number.
+	 *
+	 * @param[in]  timezone      time zone
+	 * @param[in]  utime         UTIME
+	 *
+	 * @return week day number.
+	 *
+	 */
 	int getDayOfWeekInMonth(const char *timezone, const long long int utime);
+
+	/**
+	 * @brief Get week day name.
+	 *
+	 * @param[in]  timezone      time zone
+	 * @param[in]  utime         UTIME
+	 *
+	 * @return week day name.
+	 *
+	 */
 	const char* getWeekdayText(const char *timezone, const long long int utime);
+
+	/**
+	 * @brief Get week day name by number.
+	 *
+	 * @param[in]  weekday      week day numbe
+	 *
+	 * @return week day name.
+	 *
+	 */
 	const char* getWeekdayText(int weekday);
+
+	/**
+	 * @brief Get week day short name by number.
+	 *
+	 * @param[in]  weekday      week day numbe
+	 *
+	 * @return week day short name.
+	 *
+	 */
 	const char* getWeekdayShortText(int weekday);
-	int getWeekday(const char *timezone, const long long int utime); // 0 : sun ~ 6 : sat
 
-	void updateTmHour(const int delta, struct tm& tm);
-	void updateTmDay(const int delta, struct tm& tm);
-	void updateTmMonth(const int delta, struct tm& tm);
-	void updateTmYear(const int delta, struct tm& tm);
+	/**
+	 * @brief Get day of week number.
+	 *
+	 * @param[in]  timezone      time zone
+	 * @param[in]  utime         UTIME
+	 *
+	 * @return day of week number, 0(sun) - 6(sat).
+	 *
+	 */
+	int getWeekday(const char *timezone, const long long int utime);
 
-	int getDayDiff(const struct tm& date1, const struct tm& date2);
+	/**
+	 * @brief Add hours to time.
+	 *
+	 * @param[in]      delta     hours delta
+	 * @param[in,out]  utime     UTIME
+	 *
+	 */
+	void updateTmHour(const int delta, struct tm &tm);
 
-	// get timezone text
-	void getDisplayTextTimeZone(const std::string& timeZone, std::string& displayText);
+	/**
+	 * @brief Add days to time.
+	 *
+	 * @param[in]      delta     hours delta
+	 * @param[in,out]  utime     UTIME
+	 *
+	 */
+	void updateTmDay(const int delta, struct tm &tm);
 
-	int getLocaleFirstDayOfWeek(); // 0 : sun ~ 6 : sat
+	/**
+	 * @brief Add months to time.
+	 *
+	 * @param[in]      delta     hours delta
+	 * @param[in,out]  utime     UTIME
+	 *
+	 */
+	void updateTmMonth(const int delta, struct tm &tm);
+
+	/**
+	 * @brief Add years to time.
+	 *
+	 * @param[in]      delta     hours delta
+	 * @param[in,out]  utime     UTIME
+	 *
+	 */
+	void updateTmYear(const int delta, struct tm &tm);
+
+
+	/**
+	 * @brief Get days difference dabetween two times.
+	 *
+	 * @param[in] date1    first time to compare
+	 * @param[in] date2    second time to compare
+	 *
+	 * @return time difference, @see difftime();
+	 *
+	 */
+	int getDayDiff(const struct tm &date1, const struct tm &date2);
+
+	/**
+	 * @brief Get display text of time zone.
+	 *
+	 * @param[in]   timezone      time zone
+	 * @param[out]  displayText   text buffer
+	 */
+	void getDisplayTextTimeZone(const std::string &timeZone, std::string &displayText);
+
+	/**
+	 * @brief Get number of the first day of week for current locale.
+	 *
+	 * @return day number, 0(sun) - 6(sat).
+	 *
+	 */
+	int getLocaleFirstDayOfWeek();
+
+	std::string __tzid;
+	i18n_ucalendar_h __cal;
+
+	std::string __locale;
+	i18n_udatepg_h __patternGenerator;
+	std::map<std::string,i18n_udate_format_h> __mapFormat;
+
+protected:
+	CalLocaleManager();
+	virtual ~CalLocaleManager();
 
 private:
+	WDISABLE_COPY_AND_ASSIGN(CalLocaleManager);
+
 	i18n_ucalendar_h __getUcal(const std::string &tzid); // __cal_locale_manager_get_ucal
 	void __getLocale(std::string &localeStr);
 
@@ -130,20 +340,13 @@ private:
 	void __setUCalendar(i18n_ucalendar_h calendar, const struct tm *tm);
 	void __getUCalendar(i18n_ucalendar_h calendar, struct tm *tm);
 	char* __getTzOffset(i18n_ucalendar_h cal);
-	char* __getTzName(i18n_ucalendar_h cal, const char *language, const std::string& timeZone);
+	char* __getTzName(i18n_ucalendar_h cal, const char *language, const std::string &timeZone);
 
 	// for weekday
 	void __initWeekday();
 
-	int __getTimezoneOffsetFormCalDb(const std::string& standardName);
+	int __getTimezoneOffsetFormCalDb(const std::string &standardName);
 
-public:
-	std::string __tzid;
-	i18n_ucalendar_h __cal;
-
-	std::string __locale;
-	i18n_udatepg_h __patternGenerator;
-	std::map<std::string,i18n_udate_format_h> __mapFormat;
 private:
 	std::string __weekdayText[7];
 	std::string __weekdayShortText[7];
